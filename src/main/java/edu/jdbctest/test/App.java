d8f0ee4d05f982be623116b1e847377f2424c18b
Maxim Hrunenko <maxhrunenko@gmail.com>
2023-07-24 23:43:35 +0300

    litle appdate, experementing with ResultSet

--- src/main/java/edu/jdbctest/test/App.java
+++ src/main/java/edu/jdbctest/test/App.java
@@ -2,6 +2,7 @@ package edu.jdbctest.test;
 
 import java.sql.Connection;
 import java.sql.Driver;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -9,18 +10,26 @@ import java.sql.Statement;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main(String[] args) throws SQLException {
         Class<Driver> driverClass = Driver.class;
         String sql = """
-                CREATE DATABASE game;
-                 """;
+           select *
+           from ticket
+                          """;
         try (Connection connection = ConnectionManager.open();
                 Statement statement = connection.createStatement()) {
+            System.out.println(connection.getSchema());
             System.out.println(connection.getTransactionIsolation());
-            boolean executeResult = statement.execute(sql);
+            ResultSet executeResult = statement.executeQuery(sql);
             System.out.println(executeResult);
+            while(executeResult.next()) {
+                System.out.println(executeResult.getLong("id"));
+                System.out.println(executeResult.getString("passenger_no"));
+                System.out.println(executeResult.getString("passenger_name"));
+                System.out.println(executeResult.getBigDecimal("cost"));
+                System.out.println("--------");
+            }
         }
     }
 }
